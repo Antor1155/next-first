afcaaa80b8fbaad1e18cc0971f7aa6c34b62b8a8
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-01 01:23:48 +0600

    updated jsconfig.json file's path value to @sysle will get style folder from root directory, updated layout to RootLayout and imported children

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "bcrypt mongodb mongoose next-auth "
+
+# [https://youtu.be/wm5gMKuwSYk]
---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center">
+      <h1 className="head_text">
+        Discover and share 
+        <br ></br>
+      </h1>
+    </section>
+  )
+}
+
+export default Home
