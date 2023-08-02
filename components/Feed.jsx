52dc7994525f121c99ee81184cc3787950341b32
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-03 05:58:29 +0600

    added promptCardList, promptCard and Feed.jsx to show all prompt, now need to make the backend api to call all prompt

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,47 @@
+"use client"
+import  {useEffect, useState} from "react"
+
+import PromptCardList from "./PromptCardList"
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState("")
+  const [posts, setPosts] = useState([])
+
+  const handleSearchChange = (e) =>{
+
+  }
+
+  useEffect(()=>{
+    const fetchPosts = async () =>{
+      const response = await fetch("/api/prompt")
+      const data = await response.json()
+
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search with #tag or User name"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        >
+        </input>
+      </form>
+
+      <PromptCardList
+        data={posts}
+      ></PromptCardList>
+    </section>
+
+
   )
 }
 
