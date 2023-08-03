5e3bfc423e9e281b03ab652b49293b370b4f634c
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-03 18:51:30 +0600

    api/prompt/[id] route to update prompt is also added

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -24,6 +24,29 @@ export const GET = async (res, {params}) =>{
 }
 
 //PATCH
+export const PATCH = async (request, {params}) =>{
+    const {prompt, tag} = await request.json()
 
+    try{
+        await connectToDB()
+
+        const existingPrompt = await Prompt.findById(params.id)
+
+        if(!existingPrompt) return new Response("Prompt not found", {
+            status: 404
+        })
+        
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag
+
+        await existingPrompt.save()
+
+        return new Response(JSON.stringify(existingPrompt), {status: 200})
+
+    }catch(error){
+        console.log(error)
+        return new Response("Failed to update prompt", {status: 500})
+    }
+}
 
 //DELETE
