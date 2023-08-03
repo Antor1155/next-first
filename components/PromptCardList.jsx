065f4de33fe5f609dc43941c283afc8d51fd996d
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-03 06:16:05 +0600

    added prompt route.js ot show posts on the home page and PromptCardList and PromptCard.jsx are now independent component

--- components/PromptCardList.jsx
+++ components/PromptCardList.jsx
@@ -1,6 +1,7 @@
 import PromptCard from "./PromptCard"
 
 const PromptCardList = ({data, handleTagClick}) =>{
+    console.log(data)
   return (
     <div className="mt-16 prompt_loyout">
       {data.map((post) =>{
